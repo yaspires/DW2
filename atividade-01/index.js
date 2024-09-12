dee28e87346439530560b3c0e835f2e592b80426
yaspires <yasmin06pb@hotmail.com>
2024-09-12 11:46:42 -0300

    criação de arrays de objetos

--- atividade-01/index.js
+++ atividade-01/index.js
@@ -8,10 +8,23 @@ app.get("/",(req,res) => {
 app.set("view engine", "ejs");
 
 app.get("/clientes",(req,res) => {
-    res.render("clientes")
+    const cliente = [
+        {nome: "Ana kuzendorff", cpf: "123.456.789.10", endereco: "Rua São Luis, 79"},
+        {nome: "Isabely Lemos", cpf: "693.678.823.06", endereco:"Rua das limeiras, 192"},
+        {nome: "Gustavo Lanna", cpf: "731.463.098.67", endereco:"Rua Aracaju, 03"},
+        {nome: "Yasmin Pires", cpf: "982.378.432.09", endereco:"Rua Santo Antônio, 232"}
+    ];
+
+    res.render("clientes", { cliente:cliente })
 })
 
 app.get("/produtos", (req,res) => {
+    const produtos = [
+        {produto:"Notebook", preco:3450, categoria:"Eletronicos"},
+        {produto:"Celular", preco:1980, categoria:"Eletronicos"},
+        {produto:"Fones de ouvido", preco:70, categoria:"Acessórios"},
+        {produto:"Mouse", preco:75, categoria:"Acessórios"}
+    ]
     res.render("produtos")
 })
 
